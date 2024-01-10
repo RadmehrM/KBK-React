50f34d8869de2add9bbc62fe4fad4c8f12506874
hbelange <harrisonbelanger@gmail.com>
2024-01-10 18:38:28 -0500

    initial commit

--- kbk-react/src/components/About.js
+++ kbk-react/src/components/About.js
@@ -1,1 +1,13 @@
-"Hello world - Harrison"
+import image1 from "../images/69ba5b_25690ae97244444f930a4f3ff90ea00b~mv2.webp";
+import image2 from "../images/69ba5b_86aa0cc93ce841509b8f3d93e2b2a771~mv2.webp";
+import "./About.css";
+
+function About(){
+    return (
+        <div className="timeline">
+            hi
+        </div>
+    )
+}
+
+export default About;
