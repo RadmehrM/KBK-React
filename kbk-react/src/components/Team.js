0699c4bc6b0914791dc59b48d871293c6474abfc
m-shahzad1 <114366406+m-shahzad1@users.noreply.github.com>
2024-01-18 18:58:46 -0500

    initial commit

---
+++ kbk-react/src/components/Team.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Team.css';
+
+const YourComponent = () => {
+  return (
+    <div className="container">
+      <h1>Your Component</h1>
+      <img
+        src="path/to/your/image.jpg"
+        alt="Description of your image"
+        className="image"
+      />
+    </div>
+  );
+};
+
+export default YourComponent;
