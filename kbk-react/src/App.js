92cd724ff38824319bbc4a0f5a18d19430b21720
m-shahzad1 <114366406+m-shahzad1@users.noreply.github.com>
2024-01-22 16:43:52 -0500

    added components to app.js

--- kbk-react/src/App.js
+++ kbk-react/src/App.js
@@ -1,28 +1,17 @@
+import React from 'react';
 import './App.css';
-import About from './components/About.js'
-
-import Location from './components/Location.js';
+import About from './components/About';
+import Location from './components/Location';
+import TeamSection from './components/TeamSection';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Location/>
-        <About/>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <Location />
+        <About />
       </header>
+      <TeamSection />
     </div>
   );
 }
