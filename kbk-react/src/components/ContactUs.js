45f604caf1058ac170560619dc352f1399d8508d
dylen400mh <dylenbelanger2004@gmail.com>
2024-02-04 11:15:49 -0500

    Set up contact form

--- kbk-react/src/components/ContactUs.js
+++ kbk-react/src/components/ContactUs.js
@@ -1,11 +1,18 @@
 import "./ContactUs.css";
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 /* TODO
  - add proper styling to meet visual expectations
- - connect frontend to formspree api to handle contact form submissions
 */
 
 function ContactUs() {
+  // state variable to display success message
+  const [state, handleSubmit] = useForm("xnqepvov");
+  if (state.succeeded) {
+    return <p>Message sent, thanks!</p>;
+  }
+
   return (
     <div className="container">
       <h1>Contact Us!</h1>
@@ -18,16 +25,29 @@ function ContactUs() {
         <img src={require("../images/ContactUs/socials.png")}></img>
       </div>
       <div className="form">
-        <form action="https://formspree.io/f/{form_id}" method="post">
-          <label for="first-name">First Name</label>
-          <input name="FirstName" id="first-name" type="text" />
-          <label for="last-name">Last Name</label>
-          <input name="LastName" id="last-name" type="text" />
-          <label for="email">Email *</label>
-          <input name="Email" id="email" type="email" required />
-          <label for="message">Message</label>
-          <textarea name="Message" id="message" type="message"></textarea>
-          <button type="submit">Send</button>
+        {/*Form endpoint goes below, will need to replace this with your own*/}
+        <form
+          onSubmit={handleSubmit}
+          action="https://formspree.io/f/xnqepvov"
+          method="post"
+        >
+          <label htmlFor="first-name">First Name</label>
+          <input name="firstName" id="first-name" type="text" />
+          <label htmlFor="last-name">Last Name</label>
+          <input name="lastName" id="last-name" type="text" />
+          <label htmlFor="email">Email *</label>
+          <input name="email" id="email" type="email" required />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message" type="message"></textarea>
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+          <button type="submit" disabled={state.submitting}>
+            Send
+          </button>
         </form>
       </div>
     </div>
