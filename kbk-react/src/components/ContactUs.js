e28c8e017173b320c0a58ab7f165ad65a3a83c65
dylen400mh <dylenbelanger2004@gmail.com>
2024-02-04 11:59:18 -0500

    Complete ContactUs page styling

--- kbk-react/src/components/ContactUs.js
+++ kbk-react/src/components/ContactUs.js
@@ -3,7 +3,7 @@ import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
 
 /* TODO
- - add proper styling to meet visual expectations
+ - add ellipses
 */
 
 function ContactUs() {
@@ -24,20 +24,28 @@ function ContactUs() {
         <span>social media</span>
         <img src={require("../images/ContactUs/socials.png")}></img>
       </div>
-      <div className="form">
-        {/*Form endpoint goes below, will need to replace this with your own*/}
-        <form
-          onSubmit={handleSubmit}
-          action="https://formspree.io/f/xnqepvov"
-          method="post"
-        >
+      {/*Form endpoint goes below, will need to replace this with your own*/}
+      <form
+        onSubmit={handleSubmit}
+        action="https://formspree.io/f/xnqepvov"
+        method="post"
+      >
+        <div className="form-control first-name">
           <label htmlFor="first-name">First Name</label>
           <input name="firstName" id="first-name" type="text" />
+        </div>
+
+        <div className="form-control last-name">
           <label htmlFor="last-name">Last Name</label>
           <input name="lastName" id="last-name" type="text" />
+        </div>
+
+        <div className="form-control email">
           <label htmlFor="email">Email *</label>
           <input name="email" id="email" type="email" required />
           <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
+        <div className="form-control message">
           <label htmlFor="message">Message</label>
           <textarea name="message" id="message" type="message"></textarea>
           <ValidationError
@@ -45,11 +53,11 @@ function ContactUs() {
             field="message"
             errors={state.errors}
           />
-          <button type="submit" disabled={state.submitting}>
-            Send
-          </button>
-        </form>
-      </div>
+        </div>
+        <button id="submit" type="submit" disabled={state.submitting}>
+          Send
+        </button>
+      </form>
     </div>
   );
 }
