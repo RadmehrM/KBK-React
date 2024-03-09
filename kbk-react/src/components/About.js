8406a163366f8312048c3d7040b561d9727e1ba8
hbelange <harrisonbelanger@gmail.com>
2024-03-08 23:38:49 -0500

    make about us page responsive (excluding nav bar and footer)

--- kbk-react/src/components/About.js
+++ kbk-react/src/components/About.js
@@ -8,33 +8,33 @@ const items = ["Kids being Kids is founded by two dedicates students, Ben Lamber
 "Since its establishment, the organization has managed to donate over $20,000 worth of sports equipment and running shoes to renowned organizations such as the Boys and Girls Club, YMCA, and Teen Challenge.",
 "Looking ahead, Kids being Kids has ambitious plans to expand their outreach efforts by securing monetary donations from generous supporters. By the end of 2023, they hope to establish a robust financial support system that will enable them to extend their reach to even more deserving children in the community."];
 
-function TimelineItem({ description, index }){
-    return (
-        <div className={`timelineItem timelineItem${index}`}>
-            <div className='timelineIcon'></div>
-            <div className="timelineContent">
-                {description}
-            </div>
-        </div>
-    )
-}
+// function TimelineItem({ description, index }){
+//     return (
+//         <div className={`timelineItem timelineItem${index}`}>
+//             <div className='timelineIcon'>lol</div>
+//             <div className="timelineContent">
+//                 {description}
+//             </div>
+//         </div>
+//     )
+// }
 
-function TimelineItems({ items }){
-    return (
-        <div className="timelineItems">
-            {items.map((item, index) => {
-                return <TimelineItem key={index} description={item} index={index}/>
-            })}
-        </div>
-    )
-}
+// function TimelineItems({ items }){
+//     return (
+//         <div className="timelineItems">
+//             {items.map((item, index) => {
+//                 return <TimelineItem key={index} description={item} index={index}/>
+//             })}
+//         </div>
+//     )
+// }
 
 
 function About(){
     return (
-        <div>
+        <div className='aboutComponent'>
             
-            <div className='header'>
+            <div className='aboutHeader'>
                 <div className='about-disp-box'>
                     <h1 id='about-title'>About Us</h1>
                     <div className='about-text-box'>
@@ -52,15 +52,31 @@ function About(){
 
             </div>
             <div className="timelineHeader">
-                <div className="headerText">Timeline . . .</div>
-                <img className="underline" src={underline}/>
+                <div className="timelineHeaderText">Timeline . . .</div>
+                <img className="timelineUnderline" src={underline}/>
             </div>
             <div className="timelineContainer">
-                <div className="timeline"></div>
-                <TimelineItems items={items}/>
+                <div className="timelineLeft">
+                    <div className="timeline-line">
+                        <div className="timelinePoint"></div>
+                    </div>
+                    <div className="timelineComponent">"Kids being Kids is founded by two dedicates students, Ben Lambert and Adam Cohen, with the shared goal of giving back to their community."</div>
+                    <div className="timeline-line">
+                        <div className="timelinePoint timelinePointMiddle"></div>
+                    </div>
+                    <div className="timelineComponent">"Ben and Adam rally their community members, including fellow students, families, and local businesses, to join their cause."</div>
+                    <div className="timeline-line">
+                        <div className="timelinePoint timelinePointMiddle"></div>
+                    </div>
+                    <div className="timelineComponent">"Since its establishment, the organization has managed to donate over $20,000 worth of sports equipment and running shoes to renowned organizations such as the Boys and Girls Club, YMCA, and Teen Challenge."</div>
+                    <div className="timeline-line">
+                        <div className="timelinePoint timelinePointBottom"></div>
+                    </div>
+                    <div className="timelineComponent">"Looking ahead, Kids being Kids has ambitious plans to expand their outreach efforts by securing monetary donations from generous supporters. By the end of 2023, they hope to establish a robust financial support system that will enable them to extend their reach to even more deserving children in the community."</div>
+                </div>
                 <div className='timelineImages'>
-                    <img className="image1" src={image1}></img>
-                    <img className="image2" src={image2}></img>
+                    <img className="timelineImage" src={image1}></img>
+                    <img className="timelineImage" src={image2}></img>
                 </div>
             </div>
         </div>
