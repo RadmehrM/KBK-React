f3af12aa853768470a86cdd0bc82423b119d5e65
SammyCohoe <scohoe@uwo.ca>
2024-03-27 22:54:50 -0400

    feat: footer is now mobile responsive

--- kbk-react/src/components/Footer.js
+++ kbk-react/src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 import nbImage from '../images/nb.webp';
 import leafsImage from '../images/leafs.webp';
 import baseballImage from '../images/baseball.webp';
@@ -8,6 +8,23 @@ import '../styles/Footer.css'
 
 
 function Footer() {
+
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth <= 450);
+      };
+  
+      window.addEventListener('resize', handleResize);
+  
+      // Call handleResize to set initial state
+      handleResize();
+  
+      // Clean up event listener on component unmount
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
   return (
     <footer >
         
@@ -27,16 +44,16 @@ function Footer() {
         </div>
             
         <div className='socials' style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <a href='mailto:kidsbeingkidscharity@gmail.com' style={{ marginRight: '28px' }}>kidsbeingkidscharity@gmail.com</a>
+            <a href='mailto:kidsbeingkidscharity@gmail.com' style={{ marginRight: isMobile? '10px' : '28px' }}>kidsbeingkidscharity@gmail.com</a>
             <SocialIcon
                 url='https://www.linkedin.com/company/kids-being-kids-charity/'
                 bgColor='transparent'
-                style={{ width: '100px', height: '100px', marginLeft: '0px' }}
+                style={{ width: isMobile ? '30px' : '100px', height: isMobile ? '30px' : '100px', marginRight: isMobile? '0px' : '10px' }}
             />
             <SocialIcon
                 url='https://www.instagram.com/kidsbeingkidsorg/'
                 bgColor='transparent'
-                style={{ width: '100px', height: '100px' }}
+                style={{ width: isMobile ? '30px' : '100px', height: isMobile ? '30px' : '100px', marginRight: isMobile? '0px' : '10px' }}
             />
         </div>
 
